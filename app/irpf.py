d34747bbaca2e6bc3eefd813432c86aa5c138a97
tacombel <tacombel@gmail.com>
2020-01-17 22:20:36 +0100

    calculo diferenciado por copia

--- app/irpf.py
+++ app/irpf.py
@@ -1,4 +1,5 @@
 import csv
+from collections import defaultdict
 
 datos = []
 
@@ -15,13 +16,38 @@ with open('data.csv', newline='') as csvfile:
             e = e.replace(',', '.', 1)
             linea.append(e)
         datos.append(linea)
-    rollover_fees_and_dividends = 0.00
-    profit = 0.00
+
+    estructura = defaultdict(dict)
+
     for e in datos:
-        rollover_fees_and_dividends = rollover_fees_and_dividends + round(float(e[13]), 2)
-        profit = profit + round(float(e[8]), 2)
-    rollover_fees_and_dividends = round(rollover_fees_and_dividends, 2)
-    profit = round(profit, 2)
-    print(len(datos), 'transacciones')
-    print('Rollover Fees And Dividends =', str(rollover_fees_and_dividends) + '$')
-    print('Profit', str(profit) + '$')
+        if not e[2] in estructura:
+            estructura[e[2]]['profit'] = round(float(e[8]), 2)
+            estructura[e[2]]['fees'] = round(float(e[13]), 2)
+            estructura[e[2]]['transacciones'] = 1
+        else:
+            estructura[e[2]]['profit'] = estructura[e[2]]['profit'] + round(float(e[8]), 2)
+            estructura[e[2]]['fees'] = estructura[e[2]]['fees'] + round(float(e[13]), 2)
+            estructura[e[2]]['transacciones'] += 1
+
+    total_transacciones = 0
+    total_profit = 0
+    total_fees = 0
+
+    print('Fecha cierre primera operación', datos[len(datos) - 1][10])
+    print('Fecha cierre última operación', datos[1][10])
+    print()
+
+    for key in estructura:
+        print(key)
+        print(' Transacciones =', estructura[key]['transacciones'])
+        print(' Profit        =', '{:.2f}'.format(estructura[key]['profit']) + '$')
+        print(' Fees          =', '{:.2f}'.format(estructura[key]['fees']) + '$')
+        print(' Neto          =', '{:.2f}'.format((estructura[key]['profit'] - estructura[key]['fees'])) + '$')
+        total_transacciones = total_transacciones + estructura[key]['transacciones']
+        total_profit = total_profit + round(estructura[key]['profit'], 2)
+        total_fees = total_fees + round(estructura[key]['fees'], 2)
+    print('-----------------------------')
+    print('Transacciones totales =', total_transacciones)
+    print('Profit total          =', '{:.2f}'.format(total_profit) + '$')
+    print('Fees totales          =', '{:.2f}'.format(total_fees) + '$')
+    print('Neto total            =', '{:.2f}'.format(total_profit - total_fees) + '$')
