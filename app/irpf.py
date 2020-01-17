e465bc1e3ba59fbc26eb89f8774d57536c4b7b04
tacombel <tacombel@gmail.com>
2020-01-17 21:34:10 +0100

    calcula fees y beneficio en dolares

--- app/irpf.py
+++ app/irpf.py
@@ -1,1 +1,27 @@
-# python 3.5
+import csv
+
+datos = []
+
+with open('data.csv', newline='') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+    primera = True
+    for row in spamreader:
+        if primera:
+            primera = False
+            continue
+
+        linea = []
+        for e in row:
+            e = e.replace(',', '.', 1)
+            linea.append(e)
+        datos.append(linea)
+    rollover_fees_and_dividends = 0.00
+    profit = 0.00
+    for e in datos:
+        rollover_fees_and_dividends = rollover_fees_and_dividends + round(float(e[13]), 2)
+        profit = profit + round(float(e[8]), 2)
+    rollover_fees_and_dividends = round(rollover_fees_and_dividends, 2)
+    profit = round(profit, 2)
+    print(len(datos), 'transacciones')
+    print('Rollover Fees And Dividends =', str(rollover_fees_and_dividends) + '$')
+    print('Profit', str(profit) + '$')
