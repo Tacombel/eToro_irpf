c8f39580c408bea2a9b266310b4f7d422db29af4
tacombel <tacombel@gmail.com>
2021-04-28 18:35:37 +0200

    elimino los builds de github

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ build_exe_options = {"packages": ["os"], "includes": ["queue"]}
 base = None
 
 setup(  name = "IRPF_eToro_2020",
-        version = "1.0",
+        version = "1.1",
         description = "Calculo del IRPF de Etoro",
         options = {"build_exe": build_exe_options},
         executables = [Executable("app/irpf.py", base=base)])
